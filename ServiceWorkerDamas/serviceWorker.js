31f32c400fbc4519d8158d21c8dc5024e7b88146
EpicJokinFail <joaquin.pb99@gmail.com>
2019-04-09 17:28:26 +0200

    Damas

---
+++ ServiceWorkerDamas/serviceWorker.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var cacheVersion = 1;
+var currentCache = {
+	offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'juego_offline.html';
+
+this.addEventListener('install', event => {
+	event.waitUntil(
+	caches.open(currentCache.offline).then(function(cache) {
+		return cache.addAll([offlineUrl]);
+	})
+	);
+});
+
