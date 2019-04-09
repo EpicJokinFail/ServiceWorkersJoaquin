d5bb6cb33895f7cd54144352da7584187db98d22
EpicJokinFail <joaquin.pb99@gmail.com>
2019-04-09 17:36:52 +0200

    a ver si ahora

--- ServiceWorkerDamas/juego.js
+++ ServiceWorkerDamas/juego.js
@@ -230,16 +230,15 @@ function iniciarJuego(canvasElement, moveCountElement) {
 
 function register_service_worker( ){
 
-	if ( 'sericeWorker' in navigator ){
-
-		navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-
-			console.log('ServiceWorker registration successful with scope: ' , registration.scope);
-
-		}).catch(function (err) {
-
-			console.log("Serviceworker registration failed: " , err);
-		});
+	// Register the service worker
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+	    // Registration was successful
+	    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+	}).catch(function(err) {
+	    // registration failed :(
+	    	console.log('ServiceWorker registration failed: ', err);
+	    }); 
 	}
 
 }
