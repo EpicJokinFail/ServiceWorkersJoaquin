c2b5404f2c5cd74cf71e06cc171b560c7b584165
EpicJokinFail <joaquin.pb99@gmail.com>
2019-04-09 18:32:25 +0200

    final

--- ServiceWorkerDamas/serviceWorker.js
+++ ServiceWorkerDamas/serviceWorker.js
@@ -6,7 +6,7 @@ var currentCache = {
 };
 const offlineUrl = 'juego_offline.html';
 
-var recursos = ['juego.js' , offlineUrl];
+var recursos = ['juego.js' , offlineUrl , 'juego.html' , 'serviceWorker.js'];
 
 function createCacheBustedRequest(url) {
 
